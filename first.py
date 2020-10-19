d8a0023cbf3bd327ae4f903935f137b938ed2ffb
MeghanaShreyas <70753681+MeghanaShreyas@users.noreply.github.com>
2020-10-19 15:48:47 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,23 @@
+age = 25;
+name = 'meg';
+print(f'{name} is {age} year old')
+print ('this is {}'.format('me'))
+my_list=[10,2,3,4]
+another = ['msjhd','kshd']
+print(my_list+another)
+print(len(my_list));  #to find the length of a string
+my_list[0]= 100;      #supports mutation
+print(my_list);
+my_list.append(9)  # to append the numbers at the list
+print(my_list)
+my_list.pop()     # this pops means removes the last character
+print(my_list)
+my_list.pop(0)    # this pops the 0th position character
+print(my_list)
+new_list = [4,3,2,1,5]   #this is to sort the numbers in ascending order
+new_list.sort()
+p=new_list
+print(p)
+print(new_list)
+new_list.reverse()
+print(new_list)
